c18491bd0da717296f6c23c1f02e2c29ea287636
Jakub <kubplon@gmail.com>
2020-09-19 15:07:49 +0200

    UserService with dao methods

--- src/main/java/pl/sda/weather/dao/UserDao.java
+++ src/main/java/pl/sda/weather/dao/UserDao.java
@@ -4,16 +4,38 @@ import pl.sda.weather.model.UserEntity;
 import pl.sda.weather.util.EntityManagerUtil;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class UserDao {
     public void save(UserEntity userEntity) {
-        final EntityManager entityManager = EntityManagerUtil.get();
-        entityManager.getTransaction().begin();
-        entityManager.persist(userEntity);
-        entityManager.getTransaction().commit();
+        EntityManager em = EntityManagerUtil.get();
+        em.getTransaction().begin();
+        em.persist(userEntity);
+        em.getTransaction().commit();
+        em.close();
     }
 
     public UserEntity findById(Long id) {
+        EntityManager em = EntityManagerUtil.get();
+        UserEntity userEntity = em.find(UserEntity.class, id);
+        em.close();
+        return userEntity;
+    }
+
+    public List<UserEntity> findAll() {
+        EntityManager em = EntityManagerUtil.get();
+        List<UserEntity> allUsers = em.createQuery("FROM UserEntity", UserEntity.class).getResultList();
+        em.close();
+        return allUsers;
+    }
+
+    public UserEntity findByLogin(String login) {
+        EntityManager em = EntityManagerUtil.get();
+        UserEntity byLogin = em.createQuery("FROM UserEntity WHERE login=:login", UserEntity.class)
+                .setParameter("login", login)
+                .getSingleResult();
+        em.close();
+        return byLogin;
 
     }
 }
--- src/main/java/pl/sda/weather/service/UserService.java
+++ src/main/java/pl/sda/weather/service/UserService.java
@@ -3,6 +3,8 @@ package pl.sda.weather.service;
 import pl.sda.weather.dao.UserDao;
 import pl.sda.weather.model.UserEntity;
 
+import java.util.List;
+
 public class UserService {
     private UserDao userDao;
 
@@ -12,5 +14,13 @@ public class UserService {
 
     public UserEntity findById(Long id) {
         return this.userDao.findById(id);
+    }
+
+    public UserEntity findByLogin(String login) {
+        return this.userDao.findByLogin(login);
+    }
+
+    public List<UserEntity> all() {
+        return this.userDao.findAll();
     }
 }
