375b20ecd3a32d3734e20ae254604448e9a6d2eb
Jakub <kubplon@gmail.com>
2020-09-19 15:22:48 +0200

    user auth

--- src/main/java/pl/sda/weather/service/UserService.java
+++ src/main/java/pl/sda/weather/service/UserService.java
@@ -4,12 +4,39 @@ import pl.sda.weather.dao.UserDao;
 import pl.sda.weather.model.UserEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public class UserService {
     private UserDao userDao;
 
+    private UserEntity loggedInUser = null;
+
     public UserService() {
         this.userDao = new UserDao();
+    }
+
+    public void logout() {
+        this.loggedInUser = null;
+    }
+
+    public Optional<UserEntity> currentUser() {
+        return Optional.ofNullable(this.loggedInUser);
+    }
+
+    public boolean login(String login, String password) {
+        UserEntity maybeUser = userDao.findByLogin(login);
+        if(maybeUser != null && maybeUser.getPassword().equals(password)) {
+            this.loggedInUser = maybeUser;
+            return true;
+        }
+        return false;
+    }
+
+    public void register(String login, String password) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setLogin(login);
+        userEntity.setPassword(password);
+        this.userDao.save(userEntity);
     }
 
     public UserEntity findById(Long id) {
